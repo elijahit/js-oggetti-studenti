6307321638cb3711a1456a48acca26154329ff25
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-03-26 17:58:44 +0100

    END: Esercizio con foreach

---
+++ script/app.js
@@ -0,0 +1,37 @@
+const oggettiStudenti = [
+  { nome: "Manuel", cognome: "Marchi", eta: 22 },
+  { nome: "Maria", cognome: "Rossi", eta: 21 },
+  { nome: "Giovanni", cognome: "Bianchi", eta: 23 },
+  { nome: "Anna", cognome: "Verdi", eta: 20 },
+  { nome: "Luca", cognome: "Russo", eta: 22 },
+  { nome: "Giulia", cognome: "Ferrari", eta: 21 },
+  { nome: "Paolo", cognome: "Esposito", eta: 24 },
+  { nome: "Sara", cognome: "Ricci", eta: 20 },
+  { nome: "Marco", cognome: "Costa", eta: 23 },
+  { nome: "Chiara", cognome: "Gallo", eta: 22 },
+  { nome: "Davide", cognome: "Ferri", eta: 21 },
+  { nome: "Laura", cognome: "Mancini", eta: 24 },
+  { nome: "Alessandro", cognome: "Galli", eta: 20 },
+  { nome: "Eleonora", cognome: "Santoro", eta: 23 },
+  { nome: "Roberto", cognome: "Mazza", eta: 22 },
+  { nome: "Valentina", cognome: "Leone", eta: 21 },
+  { nome: "Matteo", cognome: "Conti", eta: 24 },
+  { nome: "Elisa", cognome: "Caruso", eta: 20 },
+  { nome: "Filippo", cognome: "Moretti", eta: 23 },
+  { nome: "Cristina", cognome: "Pellegrino", eta: 22 }
+];
+
+// CICLO FOR OF
+// for (const student of oggettiStudenti) {
+//   console.log(student.nome, student.cognome)
+// }
+
+// CICLO FOR
+// for (let i = 0; i < oggettiStudenti.length; i++) {
+//   console.log(oggettiStudenti[i].nome, oggettiStudenti[i].cognome);
+// }
+
+// // CICLO FOR EACH
+// oggettiStudenti.forEach(student => {
+//   console.log(student.nome, student.cognome);
+// })
